a900d356241b68c0b1fbf129529d4c108caf605c
Pazoomer <jorge.zamora245103@potros.itson.edu.mx>
2024-05-05 15:45:24 -0700

    Simple

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
+app = Flask(__name__)
 
-application = Flask(__name__)
-
-@application.route('/')
-def index():
-    return 'Hello World'
-
-if __name__ == '__main__':
-    application.run(host="0.0.0.0", port=80, debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
